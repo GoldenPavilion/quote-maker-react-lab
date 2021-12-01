443e3439f20e51b3db4e910671e35f6ce51c8127
GoldenPavilion <rcorystine@gmail.com>
2021-11-30 22:49:17 -0500

    app functioning but one last error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import QuoteForm from './components/QuoteForm';
+import Quotes from './containers/Quotes'
 
 class App extends Component {
   render() {
@@ -8,14 +10,16 @@ class App extends Component {
           <h1>Quote Maker</h1>
         </div>
         <hr />
-        {/*
-          TODO:
-
-          Render QuoteForm and Quotes Components
-         */}
+        <QuoteForm />
+        <Quotes />
       </div>
     );
   }
 }
 
 export default App;
+
+// XXX renders without crashing
+// XXX always renders an h1 tag with 'Quote Maker'
+// XXX always renders a 'QuoteForm' component
+// XXX always renders a 'Quotes' components
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,42 @@
 export default (state = [], action) => {
-  return state;
+  let index;
+  let quote;
+  switch(action.type){
+    case 'ADD_QUOTE':
+      action.quote.votes = 0
+      return state.concat(action.quote);
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId)
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: quote.votes + 1 }),
+        ...state.slice(index + 1)
+      ];
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+
+      if (quote.votes > 0){
+        return [
+          ...state.slice(0, index),
+          Object.assign({}, quote, { votes: quote.votes -1 }),
+          ...state.slice(index + 1)
+        ]
+      }
+      return state;
+
+    default:
+      return state;
+  }
 }
+
+// XXX should return the initial state
+// XXX should handle ADD_QUOTE
+// XXX should handle REMOVE_QUOTE
+// should handle UPVOTE_QUOTE
+// should handle DOWNVOTE_QUOTE and decrement vote count down 1 if vote count is positive
+// XXX should handle DOWNVOTE_QUOTE and do nothing if vote count is O
